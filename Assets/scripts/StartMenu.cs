2d39b664616baa6150803e45b302dfe766c844ca
marcelovelozo15br <marcelov15.br@gmail.com>
2026-06-30 19:55:29 -0300

    cena de Creditos e ajuste do Menu

--- Assets/scripts/StartMenu.cs
+++ Assets/scripts/StartMenu.cs
@@ -7,14 +7,18 @@ public class StartMenu : MonoBehaviour
     [SerializeField] private Button startButton;    // "Novo Jogo"
     [SerializeField] private Button continueButton; // "Continuar"
     [SerializeField] private Button configButton;
+    [SerializeField] private Button creditsButton;
     [SerializeField] private Button exitButton;
+    [SerializeField] private Button returnButton;
 
     void Start()
     {
         startButton.onClick.AddListener(NewGame);
         continueButton.onClick.AddListener(ContinueGame);
         configButton.onClick.AddListener(Config);
+        creditsButton.onClick.AddListener(Credits);
         exitButton.onClick.AddListener(ExitGame);
+        returnButton.onClick.AddListener(Return);
 
         // Desativa "Continuar" se não houver save
         bool hasSave = PlayerPrefs.GetInt("checkpoint_exists", 0) == 1;
@@ -39,7 +43,17 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Config");
     }
-
+    
+    public void Credits()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    
+    public void Return()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    
     public void ExitGame()
     {
         Application.Quit();
