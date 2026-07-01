27b70725c9b9571e22b0497c5a2f18fbf1f88687
Noxis <joseeduardogimenes2009@gmail.com>
2026-06-30 21:11:54 -0300

    End level platform fall

---
+++ Assets/scripts/PlatformFall.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{    Rigidbody2D rb;
+    float fallDelay = 2f;
+    public FixedJoint2D joint;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Destroy (joint);
+        Destroy(gameObject, fallDelay);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("Fall");
+        }
+    }
+}
