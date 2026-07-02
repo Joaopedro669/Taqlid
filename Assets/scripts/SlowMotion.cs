e1c913d695783fe6bce94f754da93af31d3398d4
Noxis <joseeduardogimenes2009@gmail.com>
2026-07-02 19:21:19 -0300

    update

---
+++ Assets/scripts/SlowMotion.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Move>().speed = 9;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Move>().speed = 17;
+        }
+    }
+
+}
